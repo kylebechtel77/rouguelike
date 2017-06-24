4b8c8c498471f71699c007f267f8de5017ddc379
Aukustus <atte.niskakoski@gmail.com>
2017-06-24 18:41:30 +0300

    Initial Start

---
+++ CSharpRogueTutorial/CSharpRogueTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal = BearLib.Terminal;
+
+namespace RogueTutorial
+{
+    class Program
+    {
+        private static void Initialize()
+        {
+            Terminal.Open();
+            Terminal.Set("window: size=80x30; font: VeraMono.ttf, size=12");
+        }
+
+        private static void MainLoop()
+        {
+            while (true)
+            {
+                Terminal.Clear();
+                Terminal.PutExt(0, 0, 0, 0, '@');
+                Terminal.Refresh();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Initialize();
+            MainLoop();
+        }
+    }
+}
